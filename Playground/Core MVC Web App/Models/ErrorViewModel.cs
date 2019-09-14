f447010df6f38e1e1e5f21a9d31865d78c43b188
Tiago Martins <TiagoPT@users.noreply.github.com>
2019-09-14 22:39:51 +0200

    Adds initial solution and projects

---
+++ Playground/Core MVC Web App/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Core_MVC_Web_App.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
